424ae040ec7b0cc49967bc4fc7d190b21272100e
Igor Karpov <igor.a.karpov@gmail.com>
2012-07-12 14:48:20 +0200

    minor changes

---
+++ util/crc8_basic.c
@@ -0,0 +1,16 @@
+#include <avr/pgmspace.h>
+
+uint8_t crc8 (register uint8_t crc, register uint8_t n)
+{
+  n ^= crc;
+  crc = 0;
+  if( n & 0x01 ) crc  = 0x5E;
+  if( n & 0x02 ) crc ^= 0xBC;
+  if( n & 0x04 ) crc ^= 0x61;
+  if( n & 0x08 ) crc ^= 0xC2;
+  if( n & 0x10 ) crc ^= 0x9D;
+  if( n & 0x20 ) crc ^= 0x23;
+  if( n & 0x40 ) crc ^= 0x46;
+  if( n & 0x80 ) crc ^= 0x8C;
+  return crc;
+}
