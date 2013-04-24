791adba87c82e0e68c42bc7231af9d6cc603b51d
Igor Karpov <igor.a.karpov@gmail.com>
2013-04-24 19:43:44 +0200

    net-terminal: added test-terminal-systimer0 firmware

---
+++ target/net-terminal/firmware/test-terminal-systimer0/__all__.c
@@ -0,0 +1,11 @@
+// Firmware
+#include "main.c"
+
+// Library
+#include "cpu/avr/timer0.c"
+
+#include "cpu/avr/drivers/display/mt12864/terminal.c"
+#include "cpu/avr/drivers/display/mt12864/text-output.c"
+#include "cpu/avr/drivers/display/mt12864/bitblt.c"
+#include "cpu/avr/drivers/display/mt12864/driver.c"
+#include "util/font/Font_6x8.c"
---
+++ target/net-terminal/firmware/test-terminal-systimer0/main.c
@@ -0,0 +1,49 @@
+#include "cpu/avr/drivers/display/mt12864/terminal.h"
+#include "system_timer.h"
+#include <avr/interrupt.h>
+
+
+// =============================================================================
+// Services binding
+// =============================================================================
+
+/**
+ * Called on every system tick.
+ */
+INLINE void system_timer__out__run(void) {
+    terminal_displayChar('.');
+}
+
+// =============================================================================
+// Application lifecycle
+// =============================================================================
+
+inline static void application__init(void) {
+    // Drivers
+    terminal_init();
+    system_timer__init();
+}
+
+
+inline static void application__start(void) {
+    terminal_displayChar('!');
+    system_timer__start();
+}
+
+inline static void application__stop(void) {
+    system_timer__stop();
+}
+
+
+// =============================================================================
+// Entry point
+// =============================================================================
+
+int main(void) {
+    application__init();
+    application__start();
+
+    sei();
+    for(;;);
+    return 0;
+}
