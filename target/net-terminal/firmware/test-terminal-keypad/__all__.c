19490a9ca9c4280df202b73ed8d80869ef86951f
Igor Karpov <igor.a.karpov@gmail.com>
2013-04-25 21:28:42 +0200

    net-terminal: added test-terminal-keypad firmware

---
+++ target/net-terminal/firmware/test-terminal-keypad/__all__.c
@@ -0,0 +1,13 @@
+// Firmware
+#include "main.c"
+
+#include "drivers/keypad.c"
+
+// Library
+#include "cpu/avr/timer0.c"
+
+#include "cpu/avr/drivers/display/mt12864/terminal.c"
+#include "cpu/avr/drivers/display/mt12864/text-output.c"
+#include "cpu/avr/drivers/display/mt12864/bitblt.c"
+#include "cpu/avr/drivers/display/mt12864/driver.c"
+#include "util/font/Font_6x8.c"
