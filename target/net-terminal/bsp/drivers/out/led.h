afe90dafc8eb6e50311a224ac61a162e7e75d8c0
Igor Karpov <igor.a.karpov@gmail.com>
2013-04-25 09:41:08 +0200

    net-terminal: fixed lcd driver

--- target/net-terminal/bsp/drivers/out/led.h
+++ target/net-terminal/bsp/drivers/out/led.h
@@ -7,16 +7,16 @@
 #define __DRIVERS__OUT__LED_H
 
 #include "can_selector.h"
-#include "cpu/avr/drivers/net/can/mcp251x/bitdefs.h.h"
+#include "cpu/avr/drivers/net/can/mcp251x/bitdefs.h"
 #include "cpu/avr/drivers/net/can/mcp251x/operations.h"
 
 
-inline void led__on(void) {
+INLINE void led__on(void) {
     can_selector__run(mcp251x_write_one_byte(MCP251X_REGISTER_BFPCTRL, (1<<MCP251X_B0BFS)|(1<<MCP251X_B0BFE)));
 }
 
 
-inline void led__off(void) { 
+INLINE void led__off(void) { 
     can_selector__run(mcp251x_write_one_byte(MCP251X_REGISTER_BFPCTRL, 0));
 }
 
