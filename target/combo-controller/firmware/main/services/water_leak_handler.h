bc077cb4193e68bcd56f8dcb71c91681451a953b
Igor Karpov <igor.a.karpov@gmail.com>
2013-04-26 13:03:00 +0200

    combo-controller: improving water leak handler configuration

--- target/combo-controller/firmware/main/services/water_leak_handler.h
+++ target/combo-controller/firmware/main/services/water_leak_handler.h
@@ -16,17 +16,22 @@
 #include "flags/notifications__pending.h"
 
 
-uint8_t EEMEM ee__water_leak_handler__enabled = WATER_LEAK_HANDLER__ENABLED;
-uint8_t water_leak_handler__enabled;
+uint8_t EEMEM ee__water_leak_handler__action__enabled = WATER_LEAK_HANDLER__ACTION__ENABLED;
+uint8_t water_leak_handler__action__enabled;
+
+uint8_t EEMEM ee__water_leak_handler__sms__enabled = WATER_LEAK_HANDLER__SMS__ENABLED;
+uint8_t water_leak_handler__sms__enabled;
+
 
 inline static void water_leak_handler__init(void) {
-    water_leak_handler__enabled = eeprom_read_byte(&ee__water_leak_handler__enabled);
+    water_leak_handler__action__enabled = eeprom_read_byte(&ee__water_leak_handler__action__enabled);
+    water_leak_handler__sms__enabled = eeprom_read_byte(&ee__water_leak_handler__sms__enabled);
 }
 
 
 /** Invoked by water_leak_sensors_scanner__run() when the state of water leak sensors has changed */
 INLINE void water_leak_handler__run(void) {
-    if (water_leak_handler__enabled && water_leak_sensors_scanner__is_active()) {
+    if (water_leak_handler__action__enabled && water_leak_sensors_scanner__is_active()) {
         out__water_valve__on();  // not switched off automatically, must be switched off manually
     }
 }
