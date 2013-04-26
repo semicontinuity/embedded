deedff159d92ff64bb0d74e429a7eb093ebdc592
Igor Karpov <igor.a.karpov@gmail.com>
2013-04-26 16:44:41 +0200

    combo-controller: configurable password in eeprom

--- target/combo-controller/firmware/main/services/alarm__auth.c
+++ target/combo-controller/firmware/main/services/alarm__auth.c
@@ -1,5 +1,8 @@
 #include "alarm__auth.h"
 
-EEMEM struct alarm__auth__password ee__alarm__auth__password;
+EEMEM struct alarm__auth__password ee__alarm__auth__password = {
+    .length = ALARM__AUTH__PASSWORD__LENGTH,
+    .data   = {ALARM__AUTH__PASSWORD__DATA}
+};
 
 struct alarm__auth__password alarm__auth__password;
