2b99e75c9829158c1ed4d0cce2ee1a0e77da4309
igorkarpov <P@ssw0rd>
2014-09-17 16:31:05 +0400

    mfrc522 test - migrate to c++ api

--- target/usbaspmod/firmware/test_mfrc522/main.cpp
+++ target/usbaspmod/firmware/test_mfrc522/main.cpp
@@ -2,8 +2,7 @@
 
 #include <stdbool.h>
 #include <stdint.h>
-//#include <avr/io.h>
-#include "arch/avr/io.h"
+#include "arch/avr/mach.hpp"
 
 #include "cpu/avr/abi.hpp"
 #include "cpu/avr/drivers/comm/mfrc522.hpp"
@@ -16,37 +15,6 @@
 
 #include <stdio.h>
 
-
-
-
-template<uint16_t ADDRESS, uint8_t BIT>
-class bit {
-public:
-//    static bool get(void) { return *BYTE::value() & (1 <<  BIT); }
-//    static void set(bool value) { if (value) *BYTE::value() |= (1<<BIT); else *BYTE::value() &= ~(1<<BIT);}
-
-//    static volatile uint8_t *host(void)       { return (volatile uint8_t *)HOST; }
-//    static bool get(void)       { return *((volatile uint8_t *)HOST) & (1 <<  BIT); }
-    static void set(bool value) { if (value) *((volatile uint8_t *)ADDRESS) |= (1<<BIT); else *((volatile uint8_t *)ADDRESS) &= ~(1<<BIT);}
-//    static void set1(bool value) { if (value) (*host()) |= (1<<BIT); else (*host()) &= ~(1<<BIT);}
-//    static void set1(bool value) { if (value) (*host()) |= (1<<BIT); else (*host()) &= ~(1<<BIT);}
-
-};
-
-
-template<uint16_t ADDRESS, uint8_t BIT>
-class pin
-{
-public:
-    
-//    static bool get(void) { return *PORT::in_reg_ptr() & (1 <<  PIN); }
-    static void set(bool value) { if (value) *((volatile uint8_t *)ADDRESS) |= (1<<BIT); else *((volatile uint8_t *)ADDRESS) &= ~(1<<BIT);}
-//    static void set(bool value) { if (value) *PORT::out_reg_ptr() |= (1<<PIN); else *PORT::out_reg_ptr() &= ~(1<<PIN);}
-
-    typedef bit<ADDRESS + 0, BIT> in;
-    typedef bit<ADDRESS + 1, BIT> dir;
-    typedef bit<ADDRESS + 2, BIT> out;
-};
 
 
 class spi {
@@ -86,12 +54,14 @@ typedef usart0<USART0__BAUD_RATE> console;
 
 typedef mfrc522<
     spi,
-    pin<MFRC522__SELECT__PORT, MFRC522__SELECT__PIN>::out,
-    pin<MFRC522__RESET__PORT, MFRC522__RESET__PIN>::out
+    MFRC522__SELECT::out,
+    MFRC522__RESET::out
 > nfc;
 
 
 void main(void) {
+    MFRC522__SELECT::set_mode(pin::mode::OUTPUT);
+    MFRC522__RESET::set_mode(pin::mode::OUTPUT);
     spi__init(SPI_CLKDIV_16);
 
     console::init();
