ac036d94d61e5bb42474b89434f6141fc2d8e1c6
igorkarpov <P@ssw0rd>
2015-02-04 19:50:23 +0300

    improved int0 driver

--- cpu/avr/int0.h
+++ cpu/avr/int0.h
@@ -12,16 +12,29 @@
 #if defined(__AVR_ATmega48__) || defined(__AVR_ATmega88__) ||\
     defined(__AVR_ATmega168__) 
 
+#define INT0__PORT              (PORTD)
+#define INT0__PIN               (2)
 #define INT0__ENABLED__HOST     (EIMSK)
 #define INT0__CONFIG__HOST      (EICRA)
 
 #elif defined(__AVR_ATmega8__) || defined(__AVR_ATmega16__) || defined(__AVR_ATmega8535__)
 
+#define INT0__PORT              (PORTD)
+#define INT0__PIN               (2)
 #define INT0__ENABLED__HOST     (GICR)
 #define INT0__CONFIG__HOST      (MCUCR)
 
 #elif defined(__AVR_AT90S8535__) || defined(__AVR_AT90S2313__)
 
+#define INT0__PORT              (PORTD)
+#define INT0__PIN               (2)
+#define INT0__ENABLED__HOST     (GIMSK)
+#define INT0__CONFIG__HOST      (MCUCR)
+
+#elif defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+
+#define INT0__PORT              (PORTB)
+#define INT0__PIN               (2)
 #define INT0__ENABLED__HOST     (GIMSK)
 #define INT0__CONFIG__HOST      (MCUCR)
 
@@ -35,7 +48,7 @@
 
 INLINE void int0__init(void) {
 #ifdef INT0__PULLUP
-    PORTD |= (1<<2); // INT0 is on PD2 pin
+    INT0__PORT |= (1<<INT0__PIN);
 #endif
     // interrupt from INT0 pin, falling edge
     INT0__CONFIG__HOST = (INT0__CONFIG__HOST | (1<<ISC01)) & ~(1<<ISC00);
