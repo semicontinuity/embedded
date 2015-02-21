6d9a73cdad4ef22c75d04571518d095f199b367d
igorkarpov <P@ssw0rd>
2015-02-21 14:35:17 +0300

    added test_display_uart_echo

--- target/plc-6u-c/firmware/test_display_uart_echo/main.c
+++ target/plc-6u-c/firmware/test_display_uart_echo/main.c
@@ -7,11 +7,12 @@
 #include "cpu/avr/usart0__rx_polled.h"
 #include "cpu/avr/usart0__tx_polled.h"
 
+#include "drivers/out/rts.h"
+
 #include <avr/interrupt.h>
-#include <avr/sleep.h>
 #include <stdbool.h>
 #include <stdint.h>
-
+#include <util/delay.h>
 
 uint8_t bytes_received;
 
@@ -22,14 +23,19 @@ int main(void) {
     display__init();
     display__render_packed(0);
 
-    usart0__rate__set(USART_BAUD_RATE);
+    usart0__init();
     usart0__rx__enabled__set(1);    
     usart0__tx__enabled__set(1);    
 
     for(;;) {
         uint8_t c = usart0__in__read();
-        display__render_packed(++bytes_received);
+        display__render_packed(c);
+
+        _delay_ms(30);
+
+        rts__set(1);
         usart0__out__write(c);
+        rts__set(0);
     }
 
     return 0;
