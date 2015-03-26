377c8714403f88705389a4bc9fa643ae35057a41
igorkarpov <P@ssw0rd>
2015-03-26 18:02:01 +0300

    ds18x20.hpp

---
+++ cpu/avr/drivers/comm/ds18x20.hpp
@@ -0,0 +1,21 @@
+// =============================================================================
+// DS18X20 driver.
+// =============================================================================
+
+#ifndef CPU__AVR__DRIVERS__COMM__DS18X20_HPP
+#define CPU__AVR__DRIVERS__COMM__DS18X20_HPP
+
+#include <stdint.h>
+#include <stdbool.h>
+
+
+
+template<typename OWBUS>
+class ds18x20 {
+
+public:
+
+};
+
+
+#endif
