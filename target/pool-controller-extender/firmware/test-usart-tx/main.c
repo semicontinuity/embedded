61a4c9fd93730e0854fb273aacf25de0b8074182
igorkarpov <P@ssw0rd>
2015-05-29 17:12:10 +0300

    pce += test-usart-tx

--- target/pool-controller-extender/firmware/test-usart-tx/main.c
+++ target/pool-controller-extender/firmware/test-usart-tx/main.c
@@ -12,12 +12,12 @@
 // =============================================================================
 
 static void application__init(void) {
+    ENABLE_PULLUP(USART0__RX);
     usart0__init();
 }
 
 static void application__start(void) {
     usart0__tx__enabled__set(1);
-    usart0__rx__enabled__set(1);
 }
 
 
@@ -31,8 +31,6 @@ int main(void) {
     for(;;) {
         usart0__out__write('A');
         _delay_ms(200);
-//        uint8_t c = usart0__in__read();
-//        usart0__out__write(c);
     }
 
     return 0;
