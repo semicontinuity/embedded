8482986ad04b8912797a0433630d9d6688b33800
igorkarpov <P@ssw0rd>
2015-06-21 18:31:37 +0300

    removed onewire-test

--- target/plc-6u-c/firmware/onewire-test/__all__.c
+++
@@ -1,9 +0,0 @@
-#include "globals.c"
-#include "main.c"
-#include "onewire__t0t2.c"
-#include "onewire__bus__inverted_buffer.c"
-#include "util/crc8_ow_unrolled.c"
-#include "cpu/avr/drivers/display/segment/glyphs__digits__hex.c"
-#include "cpu/avr/drivers/display/segment/static2.c"
-
-#include "cpu/avr/util/debug.c"
--- target/plc-6u-c/firmware/onewire-test/globals.c
+++
@@ -1,4 +0,0 @@
-#include "util/bitops.h"
-#include "cpu/avr/gpio.h"
-
-DEFINE_BITVAR(onewire__thread__alive, SIGNAL_PORT_REG(DISPLAY_DIGIT0), SIGNAL_PIN(DISPLAY_SEGMENT_DP));
