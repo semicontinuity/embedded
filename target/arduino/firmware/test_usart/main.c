b5a7e95ba1b2bd133c59f8e1fe3c38eb321f59da
igorkarpov <P@ssw0rd>
2015-09-08 18:35:23 +0300

    arduino/test_usart

---
+++ target/arduino/firmware/test_usart/main.c
@@ -0,0 +1,55 @@
+// =============================================================================
+// USART to soft SPI relay.
+// =============================================================================
+
+#include <avr/io.h>
+#include <avr/sleep.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#include "cpu/avr/gpio.h"
+#include "cpu/avr/usart0.h"
+#include "cpu/avr/usart0__rx_polled.h"
+#include "cpu/avr/usart0__tx_polled.h"
+
+
+void application__init(void) {
+    usart0__init();
+}
+
+void application__start(void) {
+    usart0__rx__enabled__set(1);
+    usart0__tx__enabled__set(1);
+}
+
+
+// main
+// -----------------------------------------------------------------------------
+int main(void) __attribute__ ((naked));
+int main(void) {
+    DDRB |= _BV(5);
+    application__init();
+    application__start();
+
+#if !defined(__AVR_ARCH__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+#endif
+    for(;;) {
+        uint8_t c = usart0__in__read();
+        PINB |= _BV(5);
+        usart0__out__write(c);
+    }
+#if !defined(__AVR_ARCH__)
+#pragma clang diagnostic pop
+#endif
+
+#if !defined(__AVR_ARCH__)
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "OCDFAInspection"
+#endif
+    return 0;
+#if !defined(__AVR_ARCH__)
+#pragma clang diagnostic pop
+#endif
+}
