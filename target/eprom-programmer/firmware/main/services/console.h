334031591881aafb71702e58d58c4b86ebefc7a1
igorkarpov <P@ssw0rd>
2016-01-11 10:33:47 +0300

    eprom-programmer: added main:

---
+++ target/eprom-programmer/firmware/main/services/console.h
@@ -0,0 +1,15 @@
+#ifndef __SERVICES__CONSOLE_H
+#define __SERVICES__CONSOLE_H
+
+#include "cpu/avr/usart0.h"
+
+
+inline void console__init(void) {
+    usart0__rate__set(USART0__BAUD_RATE);
+    usart0__tx__enabled__set(1);
+    usart0__rx__enabled__set(1);
+}
+
+void console__run(void);
+
+#endif
