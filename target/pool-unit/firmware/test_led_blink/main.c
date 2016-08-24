2d36f8618a521973a4bf7c45b23ee65be3261da4
igorkarpov <P@ssw0rd>
2016-08-24 12:55:28 +0300

    test_led_blink

---
+++ target/pool-unit/firmware/test_led_blink/main.c
@@ -0,0 +1,68 @@
+#include "stm32f1xx_hal.h"
+
+
+#define LED2_PIN                         GPIO_PIN_13
+#define LED2_GPIO_PORT                   GPIOC
+#define LED2_GPIO_CLK_ENABLE()           __HAL_RCC_GPIOC_CLK_ENABLE()  
+#define LED2_GPIO_CLK_DISABLE()          __HAL_RCC_GPIOC_CLK_DISABLE()  
+
+
+void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef clkinitstruct = {0};
+  RCC_OscInitTypeDef oscinitstruct = {0};
+  
+  /* Configure PLL ------------------------------------------------------*/
+  /* PLL configuration: PLLCLK = (HSI / 2) * PLLMUL = (8 / 2) * 16 = 64 MHz */
+  /* PREDIV1 configuration: PREDIV1CLK = PLLCLK / HSEPredivValue = 64 / 1 = 64 MHz */
+  /* Enable HSI and activate PLL with HSi_DIV2 as source */
+  oscinitstruct.OscillatorType  = RCC_OSCILLATORTYPE_HSI;
+  oscinitstruct.HSEState        = RCC_HSE_OFF;
+  oscinitstruct.LSEState        = RCC_LSE_OFF;
+  oscinitstruct.HSIState        = RCC_HSI_ON;
+  oscinitstruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  oscinitstruct.HSEPredivValue    = RCC_HSE_PREDIV_DIV1;
+  oscinitstruct.PLL.PLLState    = RCC_PLL_ON;
+  oscinitstruct.PLL.PLLSource   = RCC_PLLSOURCE_HSI_DIV2;
+  oscinitstruct.PLL.PLLMUL      = RCC_PLL_MUL16;
+  if (HAL_RCC_OscConfig(&oscinitstruct)!= HAL_OK)
+  {
+    /* Initialization Error */
+    while(1); 
+  }
+
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
+     clocks dividers */
+  clkinitstruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  clkinitstruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  clkinitstruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  clkinitstruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  clkinitstruct.APB1CLKDivider = RCC_HCLK_DIV2;  
+  if (HAL_RCC_ClockConfig(&clkinitstruct, FLASH_LATENCY_2)!= HAL_OK)
+  {
+    /* Initialization Error */
+    while(1); 
+  }
+}
+
+
+static GPIO_InitTypeDef  GPIO_InitStruct;
+
+int main(void) {
+  HAL_Init();    
+  SystemClock_Config();
+  LED2_GPIO_CLK_ENABLE();
+
+  GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull  = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+
+  GPIO_InitStruct.Pin = LED2_PIN;
+  HAL_GPIO_Init(LED2_GPIO_PORT, &GPIO_InitStruct);
+
+  while (1)
+  {
+    HAL_GPIO_TogglePin(LED2_GPIO_PORT, LED2_PIN);
+    HAL_Delay(100);
+  }
+}
