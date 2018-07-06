94ed926ad90409c243f3262cb4e3ea7adfd19dcb
igorkarpov <P@ssw0rd>
2018-07-06 18:34:31 +0300

    added arduino test firmwares

---
+++ target/arduino/firmware/test_timer0_overflow_flag/main.c
@@ -0,0 +1,36 @@
+// =============================================================================
+// Timer0 overflow flag test.
+// Produces square wave on the LED pin.
+// =============================================================================
+#include "cpu/avr/timer0.h"
+#include "cpu/avr/gpio.h"
+#include <avr/interrupt.h>
+
+
+void led__init(void) {
+    USE_AS_OUTPUT(LED);
+}
+
+void led__toggle(void) {
+    PORT_REG(SIGNAL_PORT(LED)) ^= SIGNAL_MASK(LED);
+}
+
+void schedule(void) {
+    timer0__value__set(0);
+    timer0__conf__set(TIMER0_CONF_PRESCALER_64|TIMER0_CONF_WGM_NORMAL);
+}
+
+
+int main(void) {
+    led__init();
+    schedule();
+
+    for(;;) {
+        if (timer0__overflow__interrupt__pending__get()) {
+            timer0__overflow__interrupt__pending__clear();
+            timer0__conf__set(TIMER0_CONF_DEFAULT);
+            led__toggle();
+            schedule();
+        }
+    }
+}
