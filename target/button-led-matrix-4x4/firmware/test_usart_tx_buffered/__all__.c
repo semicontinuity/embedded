f634064ff5a47506bfb6611717f105d7269e06d7
Igor Karpov <semicontinuity@example.com>
2019-02-26 16:53:29 +0300

    test_usart_tx_buffered

---
+++ target/button-led-matrix-4x4/firmware/test_usart_tx_buffered/__all__.c
@@ -0,0 +1,5 @@
+#include <avr/io.h>
+
+#include "cpu/avr/drivers/usart0__tx.c"
+#include "services/tx_ring_buffer.c"
+#include "main.c"
---
+++ target/button-led-matrix-4x4/firmware/test_usart_tx_buffered/main.c
@@ -0,0 +1,80 @@
+// =============================================================================
+// Test - send data to USART with buffering
+// =============================================================================
+
+#include "services/tx_ring_buffer.h"
+#include "cpu/avr/asm.h"
+#include "cpu/avr/usart0.h"
+#include "cpu/avr/drivers/usart0__tx.h"
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+// =============================================================================
+// Handlers
+// =============================================================================
+
+bool comm__tx__has_next(void) {
+    return true; // interrupt triggered only if TX buffer is not empty
+}
+
+uint8_t comm__tx__next() {
+    return tx_ring_buffer__get();
+}
+
+void comm__tx__on_done(void) {
+    // never invoked
+}
+
+// =============================================================================
+// Application
+// =============================================================================
+
+static void application__init(void) {
+    ENABLE_PULLUP(USART0__RX);
+    usart0__init();
+}
+
+static void application__start(void) {
+    tx_ring_buffer__start();
+    usart0__tx__start();
+}
+
+
+// =============================================================================
+// Entry point.
+// =============================================================================
+int main(void) {
+    application__init();
+    application__start();
+
+    sei();
+    for(;;) {
+        _delay_ms(500);
+        if (tx_ring_buffer__is_writable()) {
+            tx_ring_buffer__put('H');
+        }
+        if (tx_ring_buffer__is_writable()) {
+            tx_ring_buffer__put('e');
+        }
+        if (tx_ring_buffer__is_writable()) {
+            tx_ring_buffer__put('l');
+        }
+        if (tx_ring_buffer__is_writable()) {
+            tx_ring_buffer__put('l');
+        }
+        if (tx_ring_buffer__is_writable()) {
+            tx_ring_buffer__put('o');
+        }
+        if (tx_ring_buffer__is_writable()) {
+            tx_ring_buffer__put('!');
+        }
+        if (tx_ring_buffer__is_writable()) {
+            tx_ring_buffer__put('\r');
+        }
+        if (tx_ring_buffer__is_writable()) {
+            tx_ring_buffer__put('\n');
+        }
+    }
+
+    return 0;
+}
