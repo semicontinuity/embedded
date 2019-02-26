d32ff7fac2dd70e302ded646b5d9507b7cf0cb6c
Igor Karpov <semicontinuity@example.com>
2019-02-26 23:29:47 +0300

    test_keyboard_events: sending events via USART (untested)

--- target/button-led-matrix-4x4/firmware/test_keyboard_events/__all__.c
+++ target/button-led-matrix-4x4/firmware/test_keyboard_events/__all__.c
@@ -1,5 +1,11 @@
+#include <avr/io.h>
+
+#include "cpu/avr/drivers/usart0__tx.c"
+#include "services/tx_ring_buffer.c"
+
 #include "keyboard__interrupts.c"
 #include "cpu/avr/pin_change_int0.c"
 #include "cpu/avr/pin_change_int1.c"
 #include "cpu/avr/pin_change_int2.c"
+
 #include "main.c"
--- target/button-led-matrix-4x4/firmware/test_keyboard_events/main.c
+++ target/button-led-matrix-4x4/firmware/test_keyboard_events/main.c
@@ -1,5 +1,5 @@
 // =============================================================================
-// Test pin change interrupts
+// Test: report pin change interrupts to USART via ring buffer
 // =============================================================================
 
 #include <util/delay.h>
@@ -9,8 +9,29 @@
 #include "led2.h"
 #include "led3.h"
 
+#include "services/tx_ring_buffer.h"
+#include "cpu/avr/usart0.h"
+#include "cpu/avr/drivers/usart0__tx.h"
+
 #include "keyboard__pins.h"
 #include "keyboard__interrupts.h"
+
+
+// =============================================================================
+// Handlers
+// =============================================================================
+
+bool comm__tx__has_next(void) {
+    return true; // interrupt triggered only if TX buffer is not empty
+}
+
+uint8_t comm__tx__next() {
+    return tx_ring_buffer__get();
+}
+
+void comm__tx__on_done(void) {
+    // never invoked
+}
 
 
 void application__init(void) {
@@ -20,25 +41,38 @@ void application__init(void) {
     led2__init();
     led3__init();
 
+    usart0__init();
+
     keyboard__interrupts__init();
 }
 
 
 void application__start(void) {
+    tx_ring_buffer__start();
+    usart0__tx__start();
     keyboard__interrupts__start();
 }
 
 
 void pin_change_int0__run(void) {
     led1__toggle();
+    if (tx_ring_buffer__is_writable()) {
+        tx_ring_buffer__put('1');
+    }
 }
 
 void pin_change_int1__run(void) {
     led2__toggle();
+    if (tx_ring_buffer__is_writable()) {
+        tx_ring_buffer__put('2');
+    }
 }
 
 void pin_change_int2__run(void) {
     led3__toggle();
+    if (tx_ring_buffer__is_writable()) {
+        tx_ring_buffer__put('3');
+    }
 }
 
 
