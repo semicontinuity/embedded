cf3ff827cc33a9bf32fc256270442cdcd1c9b8aa
Igor Karpov <semicontinuity@example.com>
2019-02-26 12:32:03 +0300

    button-led-matrix-4x4: draft test_pcint - initialize PCINT

--- target/button-led-matrix-4x4/firmware/test_pcint/main.c
+++ target/button-led-matrix-4x4/firmware/test_pcint/main.c
@@ -22,13 +22,189 @@
 #include "drivers/in/button14.h"
 #include "drivers/in/button15.h"
 #include "drivers/in/button16.h"
+#include "cpu/avr/pin_change_int0.h"
+#include "cpu/avr/pin_change_int1.h"
+#include "cpu/avr/pin_change_int2.h"
+
+
+uint8_t pin_change_int0__mask(void) {
+    // Pin Change Interrupt 0 serves PB0..PB7
+    uint8_t mask = 0;
+#if CONCAT(0x, IN__BUTTON1__PORT)==0xB
+    mask |= (1 << (IN__BUTTON1__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON2__PORT)==0xB
+    mask |= (1 << (IN__BUTTON2__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON3__PORT)==0xB
+    mask |= (1 << (IN__BUTTON3__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON4__PORT)==0xB
+    mask |= (1 << (IN__BUTTON4__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON5__PORT)==0xB
+    mask |= (1 << (IN__BUTTON5__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON6__PORT)==0xB
+    mask |= (1 << (IN__BUTTON6__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON7__PORT)==0xB
+    mask |= (1 << (IN__BUTTON7__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON8__PORT)==0xB
+    mask |= (1 << (IN__BUTTON8__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON9__PORT)==0xB
+    mask |= (1 << (IN__BUTTON9__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON10__PORT)==0xB
+    mask |= (1 << (IN__BUTTON10__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON11__PORT)==0xB
+    mask |= (1 << (IN__BUTTON11__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON12__PORT)==0xB
+    mask |= (1 << (IN__BUTTON12__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON13__PORT)==0xB
+    mask |= (1 << (IN__BUTTON13__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON14__PORT)==0xB
+    mask |= (1 << (IN__BUTTON14__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON15__PORT)==0xB
+    mask |= (1 << (IN__BUTTON15__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON16__PORT)==0xB
+    mask |= (1 << (IN__BUTTON16__PIN));
+#endif
+    return mask;
+}
+
+void pin_change_int0__init(void) {
+    pin_change_int0__mask__set(pin_change_int0__mask());
+}
+
+
+uint8_t pin_change_int1__mask(void) {
+    // Pin Change Interrupt 1 serves PC0..PC6
+    uint8_t mask = 0;
+#if CONCAT(0x, IN__BUTTON1__PORT)==0xC
+    mask |= (1 << (IN__BUTTON1__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON2__PORT)==0xC
+    mask |= (1 << (IN__BUTTON2__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON3__PORT)==0xC
+    mask |= (1 << (IN__BUTTON3__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON4__PORT)==0xC
+    mask |= (1 << (IN__BUTTON4__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON5__PORT)==0xC
+    mask |= (1 << (IN__BUTTON5__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON6__PORT)==0xC
+    mask |= (1 << (IN__BUTTON6__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON7__PORT)==0xC
+    mask |= (1 << (IN__BUTTON7__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON8__PORT)==0xC
+    mask |= (1 << (IN__BUTTON8__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON9__PORT)==0xC
+    mask |= (1 << (IN__BUTTON9__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON10__PORT)==0xC
+    mask |= (1 << (IN__BUTTON10__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON11__PORT)==0xC
+    mask |= (1 << (IN__BUTTON11__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON12__PORT)==0xC
+    mask |= (1 << (IN__BUTTON12__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON13__PORT)==0xC
+    mask |= (1 << (IN__BUTTON13__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON14__PORT)==0xC
+    mask |= (1 << (IN__BUTTON14__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON15__PORT)==0xC
+    mask |= (1 << (IN__BUTTON15__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON16__PORT)==0xC
+    mask |= (1 << (IN__BUTTON16__PIN));
+#endif
+    return mask;
+}
+
+void pin_change_int1__init(void) {
+    pin_change_int1__mask__set(pin_change_int1__mask());
+}
+
+
+uint8_t pin_change_int2__mask(void) {
+    // Pin Change Interrupt 1 serves PD0..PD7
+    uint8_t mask = 0;
+#if CONCAT(0x, IN__BUTTON1__PORT)==0xD
+    mask |= (1 << (IN__BUTTON1__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON2__PORT)==0xD
+    mask |= (1 << (IN__BUTTON2__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON3__PORT)==0xD
+    mask |= (1 << (IN__BUTTON3__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON4__PORT)==0xD
+    mask |= (1 << (IN__BUTTON4__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON5__PORT)==0xD
+    mask |= (1 << (IN__BUTTON5__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON6__PORT)==0xD
+    mask |= (1 << (IN__BUTTON6__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON7__PORT)==0xD
+    mask |= (1 << (IN__BUTTON7__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON8__PORT)==0xD
+    mask |= (1 << (IN__BUTTON8__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON9__PORT)==0xD
+    mask |= (1 << (IN__BUTTON9__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON10__PORT)==0xD
+    mask |= (1 << (IN__BUTTON10__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON11__PORT)==0xD
+    mask |= (1 << (IN__BUTTON11__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON12__PORT)==0xD
+    mask |= (1 << (IN__BUTTON12__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON13__PORT)==0xD
+    mask |= (1 << (IN__BUTTON13__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON14__PORT)==0xD
+    mask |= (1 << (IN__BUTTON14__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON15__PORT)==0xD
+    mask |= (1 << (IN__BUTTON15__PIN));
+#endif
+#if CONCAT(0x, IN__BUTTON16__PORT)==0xD
+    mask |= (1 << (IN__BUTTON16__PIN));
+#endif
+    return mask;
+}
+
+void pin_change_int2__init(void) {
+    pin_change_int2__mask__set(pin_change_int2__mask());
+}
 
 
 void application__init(void) {
-    led1__init();
-    led2__init();
-    led3__init();
-
     button1__init();
     button2__init();
     button3__init();
@@ -45,6 +221,21 @@ void application__init(void) {
     button14__init();
     button15__init();
     button16__init();
+
+    led1__init();
+    led2__init();
+    led3__init();
+
+    pin_change_int0__init();
+    pin_change_int1__init();
+    pin_change_int2__init();
+}
+
+
+void application__start(void) {
+    pin_change_int0__start();
+    pin_change_int1__start();
+    pin_change_int2__start();
 }
 
 
@@ -66,6 +257,8 @@ void pin_change_int2__run(void) {
 int main(void) __attribute__ ((naked));
 int main(void) {
     application__init();
+    _delay_us(2);
+    application__start();
 
 #if !defined(__AVR_ARCH__)
 #pragma clang diagnostic push
