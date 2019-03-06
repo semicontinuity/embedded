66f41974ba6806c8932423ac570219711c3d8945
Igor Karpov <semicontinuity@example.com>
2019-03-06 14:25:12 +0300

    i2c_explorer console cleanup

--- target/rgb-keypad/firmware/i2c_explorer/services/console.c
+++ target/rgb-keypad/firmware/i2c_explorer/services/console.c
@@ -1,17 +1,24 @@
-#include "drivers/i2c_master.h"
+
 #include "services/console.h"
 #include "util/parser.h"
-#include "util/formatter.h"
 
 #include <avr/interrupt.h>
+#include "cpu/avr/usart0.h"
 #include "cpu/avr/usart0__rx_polled.h"
 #include "cpu/avr/drivers/usart0__tx.h"
 #include "services/tx_ring_buffer.h"
 
+#define CONSOLE__BUFFER_LENGTH 128
 
-uint8_t console__input_buffer[16];
+uint8_t console__input_buffer[CONSOLE__BUFFER_LENGTH];
 uint16_t console__input_length;
 
+
+void console__init(void) {
+    usart0__init();
+    usart0__tx__enabled__set(1);
+    usart0__rx__enabled__set(1);
+}
 
 void console__start(void) {
     tx_ring_buffer__start();
@@ -19,13 +26,13 @@ void console__start(void) {
 }
 
 
-inline void console__read_line(void) {
+void console__read_line(void) {
     console__input_length = 0;
     while (1) {
         uint8_t c = usart0__in__read();
 
-        if (c==13 || c==10) break;
-        if (console__input_length < 16) console__input_buffer[console__input_length++] = c;
+        if (c == 13 || c == 10) break;
+        if (console__input_length < CONSOLE__BUFFER_LENGTH) console__input_buffer[console__input_length++] = c;
     }
 }
 
@@ -37,34 +44,41 @@ void console__print(const uint8_t c) {
     sei();
 }
 
-void console__println(void) {
-    console__print('\r');
-    console__print('\n');
+
+void io__write_pin(volatile unsigned char *r, uint8_t bit_char, uint8_t value_char) {
+    uint8_t bit = (uint8_t) (bit_char - '0');
+    if (value_char == '0') {
+        *(r + 2) &= ~(1 << bit);
+        console__print_ok();
+        console__println();
+    } else if (value_char == '1') {
+        *(r + 2) |= (1 << bit);
+        console__print_ok();
+        console__println();
+    } else {
+        console__print_error();
+        console__println();
+    }
 }
 
-inline void console__print_error(void) {
-    console__print('E');
-    console__print('R');
-    console__print('R');
-    console__print('O');
-    console__print('R');
-}
 
-inline void console__print_ok(void) {
-    console__print('O');
-    console__print('K');
+void io__conf_pin(volatile unsigned char *r, uint8_t c2, uint8_t c3) {
+    uint8_t bit = (uint8_t) (c2 - '0');
+    if (c3 == 'R') {
+        // CONFIGURE BIT FOR READING: 'BnR', 'CnR', 'DnR'
+        *(r + 1) &= ~(1 << bit);
+        console__print_ok();
+        console__println();
+    } else if (c3 == 'W') {
+        // CONFIGURE BIT FOR WRITING: 'BnW', 'CnW', 'DnW'
+        *(r + 1) |= (1 << bit);
+        console__print_ok();
+        console__println();
+    } else {
+        console__print_error();
+        console__println();
+    }
 }
-
-inline void console__print_byte_as_hex(const uint8_t value) {
-    uint16_t hex = formatter__byte_to_hex_chars(value);
-    console__print((uint8_t)(hex >> 8));
-    console__print((uint8_t)(hex & 0xFF));
-}
-
-inline void console__print_bool_as_0_or_1(const uint8_t value) {
-    console__print((const uint8_t) (value == 0 ? '0' : '1'));
-}
-
 
 void console__run(void) {
     console__read_line();
@@ -78,7 +92,8 @@ void console__run(void) {
         break;
     case 'D': r = &PIND;
         break;
-    default:console__print_error();
+    default:
+        console__print_error();
         console__println();
         return;
     }
@@ -88,6 +103,7 @@ void console__run(void) {
         // READ BYTE: 'B', 'C', 'D'
         console__print_byte_as_hex(*r);
         console__println();
+        return;
     } else if (console__input_length == 2) {
         uint8_t c2 = console__input_buffer[1];
         if (c2 == 'R') {
@@ -95,45 +111,28 @@ void console__run(void) {
             *(r + 1) = 0x00;
             console__print_ok();
             console__println();
+            return;
         } else if (c2 == 'W') {
             // CONFIGURE PORT FOR WRITING: 'BW', 'CW', 'DW'
             *(r + 1) = 0xFF;
             console__print_ok();
             console__println();
+            return;
         } else if (c2 >= '0' && c2 <= '7') {
             // READ BIT: [B-D][0-7]
             uint8_t bit = (uint8_t) (c2 - '0');
             console__print_bool_as_0_or_1((const uint8_t) ((*r) & (1 << bit)));
             console__println();
-        } else {
-            console__print_error();
-            console__println();
+            return;
         }
     } else if (console__input_length == 3) {
         // CONFIGURE BIT: [B-D][0-7](R|W)
         uint8_t c2 = console__input_buffer[1];
         uint8_t c3 = console__input_buffer[2];
 
-        if (c2 < '0' || c2 > '7') {
-            console__print_error();
-            console__println();
+        if (c2 >= '0' && c2 <= '7') {
+            io__conf_pin(r, c2, c3);
             return;
-        }
-
-        uint8_t bit = (uint8_t) (c2 - '0');
-        if (c3 == 'R') {
-            // CONFIGURE BIT FOR READING: 'BnR', 'CnR', 'DnR'
-            *(r + 1) &= ~(1 << bit);
-            console__print_ok();
-            console__println();
-        } else if (c3 == 'W') {
-            // CONFIGURE BIT FOR WRITING: 'BnW', 'CnW', 'DnW'
-            *(r + 1) |= (1 << bit);
-            console__print_ok();
-            console__println();
-        } else {
-            console__print_bool_as_0_or_1((const uint8_t) ((*r) & (1 << bit)));
-            console__println();
         }
     } else if (console__input_length == 4) {
         // WRITE BIT: [B-D][0-7]=[0-1]
@@ -146,37 +145,19 @@ void console__run(void) {
                 *(r + 2) = (uint8_t) (parsed_byte & 0xFF);
                 console__print_ok();
                 console__println();
-            } else {
-                console__print_error();
-                console__println();
-            }
+                return;
+            } 
         } else if (c2 >= '0' && c2 <= '7') {
             // WRITE BIT: [B-D][0-7]=[0-1]
-            uint8_t c3 = console__input_buffer[2];
-            if (c3 == '=') {
-                uint8_t bit = (uint8_t) (c2 - '0');
-                uint8_t c4 = console__input_buffer[3];
-                if (c4 == '0') {
-                    *(r + 2) &= ~(1 << bit);
-                    console__print_ok();
-                    console__println();
-                } else if (c4 == '1') {
-                    *(r + 2) |= (1 << bit);
-                    console__print_ok();
-                    console__println();
-                } else {
-                    console__print_error();
-                    console__println();
-                }
-            } else {
-                console__print_error();
-                console__println();
+            if (console__input_buffer[2] == '=') {
+                io__write_pin(r, c2, console__input_buffer[3]);
+                return;
             }
-        } else {
-            console__print_error();
-            console__println();
         }
     }
+
+    console__print_error();
+    console__println();
 }
 
 
--- target/rgb-keypad/firmware/i2c_explorer/services/console.h
+++ target/rgb-keypad/firmware/i2c_explorer/services/console.h
@@ -1,18 +1,45 @@
 #ifndef __SERVICES__CONSOLE_H
 #define __SERVICES__CONSOLE_H
 
-#include "cpu/avr/usart0.h"
+#include "util/formatter.h"
 
 
-void console__init(void) {
-    usart0__init();
-    usart0__tx__enabled__set(1);
-    usart0__rx__enabled__set(1);
-}
+void console__init(void);
 
 void console__start(void);
 
 void console__run(void);
+
+void console__print(uint8_t c);
+
+
+void console__println(void) {
+    console__print('\r');
+    console__print('\n');
+}
+
+inline void console__print_error(void) {
+    console__print('E');
+    console__print('R');
+    console__print('R');
+    console__print('O');
+    console__print('R');
+}
+
+inline void console__print_ok(void) {
+    console__print('O');
+    console__print('K');
+}
+
+inline void console__print_byte_as_hex(const uint8_t value) {
+    uint16_t hex = formatter__byte_to_hex_chars(value);
+    console__print((uint8_t)(hex >> 8));
+    console__print((uint8_t)(hex & 0xFF));
+}
+
+inline void console__print_bool_as_0_or_1(const uint8_t value) {
+    console__print((const uint8_t) (value == 0 ? '0' : '1'));
+}
 
 
 #endif
