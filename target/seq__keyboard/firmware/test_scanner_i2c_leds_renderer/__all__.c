62c08a3cf338e0d39c5ee28c7fe57085ffb5cafb
semicontinuity <semicontinuity@github.com>
2019-04-18 13:45:11 +0300

    test_scanner_i2c_leds_renderer: initial version

---
+++ target/seq__keyboard/firmware/test_scanner_i2c_leds_renderer/__all__.c
@@ -0,0 +1,6 @@
+#include "drivers/scanner__thread__timer.c"
+#include "cpu/avr/timer0.c"
+#include "twi_slave__polled.c"
+
+#include "data.c"
+#include "main.c"
---
+++ target/seq__keyboard/firmware/test_scanner_i2c_leds_renderer/main.c
@@ -0,0 +1,158 @@
+// =============================================================================
+// Receives 4-byte messages via I2C with raw color data (1 byte per column)
+// and renders 1-bit colors on LEDS (3 bits RGB + 3 bits RGB + 1 bit button LED)
+// Column is selected for 1/4 ms = 256*8 clocks; thus timer0_ovf prescaler 8.
+// =============================================================================
+
+#include <avr/interrupt.h>
+#include "cpu/avr/util/vthreads.h"
+#include "drivers/scanner__thread__timer.h"
+#include "drivers/out/columns.h"
+#include "drivers/out/leds_rows.h"
+#include "util/delay.h"
+#include "data.h"
+
+#include <cpu/avr/twi.h>
+#include "twi_slave.h"
+#include "twi_slave_callbacks.h"
+
+
+// scanner
+// -----------------------------------------------------------------------------
+
+#ifdef SCANNER__THREAD__IP__REG
+register uint8_t *scanner__thread__ip asm(QUOTE(SCANNER__THREAD__IP__REG));
+#else
+volatile uint8_t *scanner__thread__ip;
+#endif
+
+
+void scanner__thread__init(void) {
+    VT_INIT(scanner__thread, scanner__thread__ip);
+}
+
+
+VT_FUNC(scanner__thread__function, scanner__thread__function_attrs) {
+//void scanner__thread__run(void) {
+
+//    VT_BEGIN(scanner__thread, scanner__thread__ip);
+
+    FC_RESUME(scanner__thread__ip);
+    {
+        VT_MARK(scanner__thread, BEGIN);
+        VT_MARK(scanner__thread, COLUMN0);
+
+        column3__set(0);
+        OUT(OUT__LED_ROWS, data__leds[0]);
+        column0__set(1);
+
+        VT_Z_YIELD_WITH_MARK_RETI(scanner__thread, scanner__thread__ip, COLUMN1);
+
+
+        column0__set(0);
+        OUT(OUT__LED_ROWS, data__leds[1]);
+        column1__set(1);
+
+        VT_Z_YIELD_WITH_MARK_RETI(scanner__thread, scanner__thread__ip, COLUMN2);
+
+
+        column1__set(0);
+        OUT(OUT__LED_ROWS, data__leds[2]);
+        column2__set(1);
+
+        VT_Z_YIELD_WITH_MARK_RETI(scanner__thread, scanner__thread__ip, COLUMN3);
+
+
+        column2__set(0);
+        OUT(OUT__LED_ROWS, data__leds[3]);
+        column3__set(1);
+
+        VT_Z_GOTO_RETI(scanner__thread, scanner__thread__ip, BEGIN);
+    }
+}
+
+
+// application
+// -----------------------------------------------------------------------------
+
+void application__init(void) {
+    data__leds[0] = 0b10010001;
+    data__leds[1] = 0b01001001;
+    data__leds[2] = 0b00100101;
+    data__leds[3] = 0b11111101;
+
+    // init I2C
+    PORTC |= ((1 << PINC4) | (1 << PINC5));
+    TWDR = 0;
+
+    twi__slave_address__set(0x1F);
+    twi__slave__thread__init();
+
+    led1b_row__init();
+    led1g_row__init();
+    led1r_row__init();
+
+    led2b_row__init();
+    led2g_row__init();
+    led2r_row__init();
+
+    column0__init();
+    column1__init();
+    column2__init();
+    column3__init();
+
+    scanner__thread__init();
+    scanner__thread__timer__init();
+}
+
+void application__start(void) {
+    twi__slave__thread__start();
+    scanner__thread__timer__start();
+}
+
+
+// TWI Slave callbacks
+// -----------------------------------------------------------------------------
+
+void twi__slave__on_data_byte_received(const uint8_t value) {
+}
+
+void twi__slave__on_data_reception_finished(void) {
+}
+
+void twi__slave__on_data_reception_aborted(void) {
+}
+
+
+// main
+// -----------------------------------------------------------------------------
+int main(void) __attribute__ ((naked));
+int main(void) {
+    application__init();
+    application__start();
+
+    sei();
+
+#if !defined(__AVR_ARCH__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+#endif
+    for(;;) {
+        if (twi__slave__thread__is_runnable()) {
+            twi__slave__thread__run();
+        }
+    }
+
+#if !defined(__AVR_ARCH__)
+#pragma clang diagnostic pop
+#endif
+
+#if !defined(__AVR_ARCH__)
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "OCDFAInspection"
+#endif
+    return 0;
+#if !defined(__AVR_ARCH__)
+#pragma clang diagnostic pop
+#endif
+}
