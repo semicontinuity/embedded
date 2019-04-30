caeb1d5b0086ab3fc7daf3afea9bbbc2447dc3b4
semicontinuity <semicontinuity@github.com>
2019-04-30 14:59:18 +0300

    test_leds_colors fixes

--- target/seq__keyboard/firmware/test_leds_colors/main.c
+++ target/seq__keyboard/firmware/test_leds_colors/main.c
@@ -2,26 +2,14 @@
 // Light up LEDS using simple loop
 // =============================================================================
 
-#include "drivers/out/columns.h"
-#include "drivers/out/leds_rows.h"
+#include "drivers/io_matrix__out_columns.h"
+#include "drivers/io_matrix__out_rows.h"
 #include "util/delay.h"
 
 
 void application__init(void) {
-    led1b_row__init();
-    led1g_row__init();
-    led1r_row__init();
-
-    led2b_row__init();
-    led2g_row__init();
-    led2r_row__init();
-
-    buttons__leds_row__init();
-
-    column0__init();
-    column1__init();
-    column2__init();
-    column3__init();
+    io_matrix__out_columns__init();
+    io_matrix__out_rows__init();
 }
 
 void application__start(void) {
@@ -40,56 +28,32 @@ int main(void) {
 #pragma clang diagnostic ignored "-Wmissing-noreturn"
 #endif
     for(;;) {
-        led1r_row__set(1);
-        led1g_row__set(0);
-        led1b_row__set(0);
+        io_matrix__out_rows__set(0x30);
 
-        led2r_row__set(0);
-        led2g_row__set(1);
-        led2b_row__set(1);
-
-        column0__set(1);
+        io_matrix__out__column0__set(1);
         _delay_ms(4);
-        column0__set(0);
+        io_matrix__out__column0__set(0);
 
 
-        led1r_row__set(0);
-        led1g_row__set(1);
-        led1b_row__set(0);
+        io_matrix__out_rows__set(0x28);
 
-        led2r_row__set(1);
-        led2g_row__set(0);
-        led2b_row__set(1);
-
-        column1__set(1);
+        io_matrix__out__column1__set(1);
         _delay_ms(4);
-        column1__set(0);
+        io_matrix__out__column1__set(0);
 
 
-        led1r_row__set(0);
-        led1g_row__set(0);
-        led1b_row__set(1);
+        io_matrix__out_rows__set(0x14);
 
-        led2r_row__set(1);
-        led2g_row__set(1);
-        led2b_row__set(0);
-
-        column2__set(1);
+        io_matrix__out__column2__set(1);
         _delay_ms(4);
-        column2__set(0);
+        io_matrix__out__column2__set(0);
 
 
-        led1r_row__set(1);
-        led1g_row__set(1);
-        led1b_row__set(1);
+        io_matrix__out_rows__set(0x02);
 
-        led2r_row__set(0);
-        led2g_row__set(0);
-        led2b_row__set(0);
-
-        column3__set(1);
+        io_matrix__out__column3__set(1);
         _delay_ms(4);
-        column3__set(0);
+        io_matrix__out__column3__set(0);
     }
 
 #if !defined(__AVR_ARCH__)
