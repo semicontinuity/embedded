4ee8be0aaaa9fe6b83ba4dfddb853993fdb80d4e
semicontinuity <semicontinuity@github.com>
2019-05-03 16:18:08 +0300

    fixing test_scanner_i2c_leds_renderer

--- target/seq__keyboard/firmware/test_scanner_i2c_leds_renderer/__all__.c
+++ target/seq__keyboard/firmware/test_scanner_i2c_leds_renderer/__all__.c
@@ -1,5 +1,6 @@
-#include "drivers/scanner__thread__timer.c"
+#include "drivers/io_matrix__scanner__thread__timer__timer0.c"
 #include "cpu/avr/timer0.c"
+#include "io_matrix__scanner__thread.c"
 #include "twi_slave__polled.c"
 
 #include "data.c"
