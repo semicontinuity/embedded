2b7b3b2b241d0168b5d0fb2b393f117150768551
semicontinuity <semicontinuity@github.com>
2019-05-03 15:48:01 +0300

    fixing test_scanner_leds_renderer

--- target/seq__keyboard/firmware/test_scanner_leds_renderer/__all__.c
+++ target/seq__keyboard/firmware/test_scanner_leds_renderer/__all__.c
@@ -1,5 +1,6 @@
-#include "drivers/scanner__thread__timer.c"
+#include "drivers/io_matrix__scanner__thread__timer__timer0.c"
 #include "cpu/avr/timer0.c"
 
 #include "data.c"
+#include "io_matrix__scanner__thread.c"
 #include "main.c"
--- target/seq__keyboard/firmware/test_scanner_leds_renderer/main.c
+++ target/seq__keyboard/firmware/test_scanner_leds_renderer/main.c
@@ -5,66 +5,12 @@
 
 #include <avr/interrupt.h>
 #include "cpu/avr/util/vthreads.h"
-#include "drivers/scanner__thread__timer.h"
-#include "drivers/out/columns.h"
-#include "drivers/out/leds_rows.h"
+#include "drivers/io_matrix__scanner__thread__timer.h"
+#include "drivers/io_matrix__out_columns.h"
+#include "drivers/io_matrix__out_rows.h"
 #include "util/delay.h"
 #include "data.h"
-
-
-// scanner
-// -----------------------------------------------------------------------------
-
-#ifdef SCANNER__THREAD__IP__REG
-register uint8_t *scanner__thread__ip asm(QUOTE(SCANNER__THREAD__IP__REG));
-#else
-volatile uint8_t *scanner__thread__ip;
-#endif
-
-
-void scanner__thread__init(void) {
-    VT_INIT(scanner__thread, scanner__thread__ip);
-}
-
-
-VT_FUNC(scanner__thread__function, scanner__thread__function_attrs) {
-//void scanner__thread__run(void) {
-
-//    VT_BEGIN(scanner__thread, scanner__thread__ip);
-
-    FC_RESUME(scanner__thread__ip);
-    {
-        VT_MARK(scanner__thread, BEGIN);
-        VT_MARK(scanner__thread, COLUMN0);
-
-        column3__set(0);
-        OUT(OUT__LED_ROWS, data__leds[0]);
-        column0__set(1);
-
-        VT_Z_YIELD_WITH_MARK_RETI(scanner__thread, scanner__thread__ip, COLUMN1);
-
-
-        column0__set(0);
-        OUT(OUT__LED_ROWS, data__leds[1]);
-        column1__set(1);
-
-        VT_Z_YIELD_WITH_MARK_RETI(scanner__thread, scanner__thread__ip, COLUMN2);
-
-
-        column1__set(0);
-        OUT(OUT__LED_ROWS, data__leds[2]);
-        column2__set(1);
-
-        VT_Z_YIELD_WITH_MARK_RETI(scanner__thread, scanner__thread__ip, COLUMN3);
-
-
-        column2__set(0);
-        OUT(OUT__LED_ROWS, data__leds[3]);
-        column3__set(1);
-
-        VT_Z_GOTO_RETI(scanner__thread, scanner__thread__ip, BEGIN);
-    }
-}
+#include "io_matrix__scanner__thread.h"
 
 
 // application
@@ -76,25 +22,14 @@ void application__init(void) {
     data__leds[2] = 0b00100101;
     data__leds[3] = 0b11111101;
 
-    led1b_row__init();
-    led1g_row__init();
-    led1r_row__init();
-
-    led2b_row__init();
-    led2g_row__init();
-    led2r_row__init();
-
-    column0__init();
-    column1__init();
-    column2__init();
-    column3__init();
-
-    scanner__thread__init();
-    scanner__thread__timer__init();
+    io_matrix__out_rows__init();
+    io_matrix__out_columns__init();
+    io_matrix__scanner__thread__init();
+    io_matrix__scanner__thread__timer__init();
 }
 
 void application__start(void) {
-    scanner__thread__timer__start();
+    io_matrix__scanner__thread__timer__start();
 }
 
 
