5775f4e906e5ace4fa712944be9e461ef63bc501
semicontinuity <semicontinuity@github.com>
2019-05-14 23:30:06 +0300

    seq__blm/main: splitting make files

--- target/seq__blm/firmware/main/__all__.c
+++ target/seq__blm/firmware/main/__all__.c
@@ -2,15 +2,19 @@
 
 #include "services/tx_ring_buffer.c"
 
+
+#include "I2CSlave.c"
+#include "comm__switch.c"
+
+//#include "comm_keyboard_simple.c"
+
+#include "drivers/comm/ws2812b.c"
+
+// Keyboard
 #include "cpu/avr/timer0.c"
 #include "cpu/avr/drivers/keyboard/keyboard__debounce_timer__timer0.c"
 #include "cpu/avr/drivers/keyboard/keyboard__pins__mapping.c"
 #include "cpu/avr/services/keyboard/keyboard.c"
 #include "drivers/keyboard__pins__physical.c"
-
-#include "I2CSlave.c"
-#include "comm__switch.c"
-
-#include "drivers/comm/ws2812b.c"
 
 #include "main.c"
