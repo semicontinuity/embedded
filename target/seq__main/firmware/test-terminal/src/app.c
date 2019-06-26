b524f2ba3215940964b0674fe1416e546edd54af
semicontinuity <semicontinuity@github.com>
2019-06-26 19:09:12 +0300

    seq_main += test-terminal

---
+++ target/seq__main/firmware/test-terminal/src/app.c
@@ -0,0 +1,159 @@
+// $Id$
+/*
+ * MIOS32 Tutorial #020: LCD Output
+ * see README.txt for details
+ *
+ * ==========================================================================
+ *
+ *  Copyright (C) 2009 Thorsten Klose (tk@midibox.org)
+ *  Licensed for personal non-commercial use only.
+ *  All other rights reserved.
+ * 
+ * ==========================================================================
+ */
+
+/////////////////////////////////////////////////////////////////////////////
+// Include files
+/////////////////////////////////////////////////////////////////////////////
+
+#include <mios32.h>
+#include "app.h"
+#include <string.h>
+#include <stdarg.h>
+
+//#include <seq_midi_out.h>
+
+#include "seq_terminal.h"
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called after startup to initialize the application
+/////////////////////////////////////////////////////////////////////////////
+void APP_Init(void) {
+    // initialize all LEDs
+    MIOS32_BOARD_LED_Init(0xffffffff);
+    SEQ_TERMINAL_Init(0);
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This task is running endless in background
+/////////////////////////////////////////////////////////////////////////////
+void APP_Background(void)
+{
+    MIOS32_MIDI_SendDebugMessage("STARTED");
+    while (1) {}
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called each mS from the main task which also handles DIN, ENC
+// and AIN events. You could add more jobs here, but they shouldn't consume
+// more than 300 uS to ensure the responsiveness of buttons, encoders, pots.
+// Alternatively you could create a dedicated task for application specific
+// jobs as explained in $MIOS32_PATH/apps/tutorials/006_rtos_tasks
+/////////////////////////////////////////////////////////////////////////////
+void APP_Tick(void)
+{
+  // PWM modulate the status LED (this is a sign of life)
+  u32 timestamp = MIOS32_TIMESTAMP_Get();
+  MIOS32_BOARD_LED_Set(1, (timestamp % 20) <= ((timestamp / 100) % 10));
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called each mS from the MIDI task which checks for incoming
+// MIDI events. You could add more MIDI related jobs here, but they shouldn't
+// consume more than 300 uS to ensure the responsiveness of incoming MIDI.
+/////////////////////////////////////////////////////////////////////////////
+void APP_MIDI_Tick(void)
+{
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called when a MIDI package has been received
+/////////////////////////////////////////////////////////////////////////////
+void APP_MIDI_NotifyPackage(mios32_midi_port_t port, mios32_midi_package_t midi_package)
+{
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called before the shift register chain is scanned
+/////////////////////////////////////////////////////////////////////////////
+void APP_SRIO_ServicePrepare(void)
+{
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called after the shift register chain has been scanned
+/////////////////////////////////////////////////////////////////////////////
+void APP_SRIO_ServiceFinish(void)
+{
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called when a button has been toggled
+// pin_value is 1 when button released, and 0 when button pressed
+/////////////////////////////////////////////////////////////////////////////
+void APP_DIN_NotifyToggle(u32 pin, u32 pin_value)
+{
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called when an encoder has been moved
+// incrementer is positive when encoder has been turned clockwise, else
+// it is negative
+/////////////////////////////////////////////////////////////////////////////
+void APP_ENC_NotifyChange(u32 encoder, s32 incrementer)
+{
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// This hook is called when a pot has been moved
+/////////////////////////////////////////////////////////////////////////////
+void APP_AIN_NotifyChange(u32 pin, u32 pin_value)
+{
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// Send a Mutex protected debug messages (referenced by DEBUG_MSG macro)
+/////////////////////////////////////////////////////////////////////////////
+void APP_SendDebugMessage(char *format, ...)
+{
+//    MUTEX_MIDIOUT_TAKE;
+    
+    {
+    char str[128]; // 128 chars allowed
+    va_list args;
+    
+    // failsave: if format string is longer than 100 chars, break here
+    // note that this is a weak protection: if %s is used, or a lot of other format tokens,
+    // the resulting string could still lead to a buffer overflow
+    // other the other hand we don't want to allocate too many byte for buffer[] to save stack
+    if( strlen(format) > 100 ) {
+        // exit with less costly message
+        MIOS32_MIDI_SendDebugString("(ERROR: string passed to MIOS32_MIDI_SendDebugMessage() is longer than 100 chars!\n");
+    } else {
+        // transform formatted string into string
+        va_start(args, format);
+        vsprintf(str, format, args);
+    }
+    
+    u32 len = strlen(str);
+    u8 *str_ptr = (u8 *)str;
+    int i;
+    for(i=0; i<len; ++i) {
+        *str_ptr++ &= 0x7f; // ensure that MIDI protocol won't be violated
+    }
+    
+    MIOS32_MIDI_SendDebugString(str);
+    }
+    
+//MUTEX_MIDIOUT_GIVE;
+}
---
+++ target/seq__main/firmware/test-terminal/src/seq_terminal.c
@@ -0,0 +1,260 @@
+/*
+ * Based on MIDIbox SEQ MIDI Terminal, by Thorsten Klose (tk@midibox.org)
+ */
+
+/////////////////////////////////////////////////////////////////////////////
+// Include files
+/////////////////////////////////////////////////////////////////////////////
+
+#include <mios32.h>
+#include <string.h>
+
+#include <app_lcd.h>
+
+#include "app.h"
+#include "seq_terminal.h"
+
+extern void vPortMallocDebugInfo(void);
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Local defines
+/////////////////////////////////////////////////////////////////////////////
+
+#define STRING_MAX 100 // recommended size for file transfers via FILE_BrowserHandler()
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Local variables
+/////////////////////////////////////////////////////////////////////////////
+
+static char line_buffer[STRING_MAX];
+static u16 line_ix;
+
+/////////////////////////////////////////////////////////////////////////////
+// Local prototypes
+/////////////////////////////////////////////////////////////////////////////
+
+static s32 TERMINAL_ParseFilebrowser(mios32_midi_port_t port, char byte);
+
+//static s32 TERMINAL_BrowserUploadCallback(char *filename);
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Initialisation
+/////////////////////////////////////////////////////////////////////////////
+s32 SEQ_TERMINAL_Init(u32 mode) {
+    // install the callback function which is called on incoming characters from MIOS Terminal
+    MIOS32_MIDI_DebugCommandCallback_Init(SEQ_TERMINAL_Parse);
+
+    // install the callback function which is called on incoming characters from MIOS Filebrowser
+//  MIOS32_MIDI_FilebrowserCommandCallback_Init(TERMINAL_ParseFilebrowser);
+
+    // clear line buffer
+    line_buffer[0] = 0;
+    line_ix = 0;
+
+    return 0; // no error
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// help function which parses a decimal or hex value
+// returns >= 0 if value is valid
+// returns -1 if value is invalid
+/////////////////////////////////////////////////////////////////////////////
+static s32 get_dec(char *word) {
+    if (word == NULL)
+        return -1;
+
+    char *next;
+    long l = strtol(word, &next, 0);
+
+    if (word == next)
+        return -1;
+
+    return l; // value is valid
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Parser
+/////////////////////////////////////////////////////////////////////////////
+s32 SEQ_TERMINAL_Parse(mios32_midi_port_t port, char byte) {
+    // temporary change debug port (will be restored at the end of this function)
+    mios32_midi_port_t prev_debug_port = MIOS32_MIDI_DebugPortGet();
+    MIOS32_MIDI_DebugPortSet(port);
+
+    if (byte == '\r') {
+        // ignore
+    } else if (byte == '\n') {
+        SEQ_TERMINAL_ParseLine(line_buffer, APP_SendDebugMessage);
+        line_ix = 0;
+        line_buffer[line_ix] = 0;
+    } else if (line_ix < (STRING_MAX - 1)) {
+        line_buffer[line_ix++] = byte;
+        line_buffer[line_ix] = 0;
+    }
+
+    // restore debug port
+    MIOS32_MIDI_DebugPortSet(prev_debug_port);
+
+    return 0; // no error
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Parser for a complete line - also used by shell.c for telnet
+/////////////////////////////////////////////////////////////////////////////
+s32 SEQ_TERMINAL_ParseLine(char *input, void *_output_function) {
+    void (*out)(char *format, ...) = _output_function;
+    char *separators = " \t";
+    char *brkt;
+    char *parameter;
+
+//  MUTEX_MIDIOUT_TAKE;
+
+
+//    if (APP_LCD_TerminalParseLine(input, _output_function) >= 1)
+//        return 0; // command parsed
+
+
+//  MUTEX_MIDIOUT_GIVE;
+
+    if ((parameter = strtok_r(line_buffer, separators, &brkt))) {
+        if (strcmp(parameter, "help") == 0) {
+            SEQ_TERMINAL_PrintHelp(out);
+        } else if (strcmp(parameter, "system") == 0) {
+            SEQ_TERMINAL_PrintSystem(out);
+        } else if (strcmp(parameter, "memory") == 0) {
+            // new: expert option (therefore not documented in help page):
+            // "memory <from-address> <to-address> dumps any memory region (not protected against bus errors - potential hard fault!)
+            char *arg;
+            if ((arg = strtok_r(NULL, separators, &brkt))) {
+                int begin_addr = get_dec(arg);
+                int end_addr = -1;
+
+                if ((arg = strtok_r(NULL, separators, &brkt))) {
+                    end_addr = get_dec(arg);
+                }
+
+                if (begin_addr == -1 || end_addr == -1) {
+                    out("SYNTAX: memory <begin-addr> <end-addr>");
+                } else if (begin_addr > end_addr) {
+                    out("ERROR: end address has to be greater equal the start address");
+                } else {
+                    u32 size = end_addr - begin_addr;
+                    if (size > 0x10000) {
+                        out("ERROR: it isn't recommended to dump more than 64k at once!");
+                    } else {
+                        u8 *ptr = (u8 *) begin_addr;
+                        MIOS32_MIDI_SendDebugHexDump(ptr, size);
+                    }
+                }
+            } else {
+                SEQ_TERMINAL_PrintMemoryInfo(out);
+            }
+        } else if (strcmp(parameter, "global") == 0) {
+            SEQ_TERMINAL_PrintGlobalConfig(out);
+        } else if (strcmp(parameter, "reset") == 0) {
+            MIOS32_SYS_Reset();
+        } else {
+            out("Unknown command - type 'help' to list available commands!");
+        }
+    }
+
+    return 0; // no error
+}
+
+
+s32 SEQ_TERMINAL_PrintHelp(void *_output_function) {
+    void (*out)(char *format, ...) = _output_function;
+
+    out("Welcome to " MIOS32_LCD_BOOT_MSG_LINE1 "!");
+    out("Following commands are available:");
+    out("  system:         print system info");
+    out("  memory:         print memory allocation info");
+    out("  sdcard:         print SD Card info");
+    out("  sdcard_format:  formats the SD Card (you will be asked for confirmation)");
+    out("  global:         print global configuration");
+    out("  config:         print local session configuration");
+    out("  tracks:         print overview of all tracks");
+    out("  track <track>:  print info about a specific track");
+    out("  mixer:          print current mixer map");
+    out("  song:           print current song info");
+    out("  grooves:        print groove templates");
+    out("  bookmarks:      print bookmarks");
+    out("  router:         print MIDI router info");
+    out("  tpd <string>:   print a scrolled text on the TPD");
+#ifndef MBSEQV4L
+    out("  lcd <string>:   print a message on LCD");
+#endif
+    out("  reset:          resets the MIDIbox SEQ (!)");
+    out("  help:           this page");
+
+    return 0; // no error
+}
+
+
+s32 SEQ_TERMINAL_PrintSystem(void *_output_function) {
+    void (*out)(char *format, ...) = _output_function;
+    char str_buffer[128];
+
+    out("System Informations:");
+    out("====================");
+    out(MIOS32_LCD_BOOT_MSG_LINE1 " " MIOS32_LCD_BOOT_MSG_LINE2 "");
+
+    mios32_sys_time_t t = MIOS32_SYS_TimeGet();
+    int hours = (t.seconds / 3600) % 24;
+    int minutes = (t.seconds % 3600) / 60;
+    int seconds = (t.seconds % 3600) % 60;
+
+    out("Operating System: MIOS32");
+    out("Board: " MIOS32_BOARD_STR "");
+    out("Chip Family: " MIOS32_FAMILY_STR "");
+    if (MIOS32_SYS_SerialNumberGet((char *) str_buffer) >= 0)
+        out("Serial Number: %s\n", str_buffer);
+    else
+        out("Serial Number: ?");
+    out("Flash Memory Size: %d bytes\n", MIOS32_SYS_FlashSizeGet());
+    out("RAM Size: %d bytes\n", MIOS32_SYS_RAMSizeGet());
+
+
+    out("Systime: %02d:%02d:%02d\n", hours, minutes, seconds);
+
+#if !defined(MIOS32_FAMILY_EMULATION) && (configGENERATE_RUN_TIME_STATS || configUSE_TRACE_FACILITY)
+    // send Run Time Stats to MIOS terminal
+    out("FreeRTOS Task RunTime Stats:");
+    FREERTOS_UTILS_RunTimeStats();
+#endif
+
+    out("done.");
+
+    return 0; // no error
+}
+
+
+s32 SEQ_TERMINAL_PrintGlobalConfig(void *_output_function) {
+    void (*out)(char *format, ...) = _output_function;
+
+    out("Global Configuration:");
+    out("=====================");
+
+    out("done.");
+
+    return 0; // no error
+}
+
+s32 SEQ_TERMINAL_PrintMemoryInfo(void *_output_function) {
+#if defined(MIOS32_FAMILY_EMULATION)
+    void (*out)(char *format, ...) = _output_function;
+    out("Not supported in emulation!");
+#else
+//  MUTEX_MIDIOUT_TAKE;
+    vPortMallocDebugInfo();
+//  MUTEX_MIDIOUT_GIVE;
+#endif
+
+    return 0; // no error
+}
+
