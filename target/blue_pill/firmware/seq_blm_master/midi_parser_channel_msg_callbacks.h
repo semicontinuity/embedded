d2a16da64b9ab98b7a6725011665edb997ce99d9
semicontinuity <igor.a.karpov@gmail.com>
2020-01-22 17:03:49 +0300

    seq_blm_master: basic midi parser

---
+++ target/blue_pill/firmware/seq_blm_master/midi_parser_channel_msg_callbacks.h
@@ -0,0 +1,3 @@
+#include "midi_package.h"
+
+void midi_channel_msg(midi_package_t midi_package);
--- target/blue_pill/firmware/seq_blm_master/serial_midi_receiver_pt.h
+++ target/blue_pill/firmware/seq_blm_master/serial_midi_receiver_pt.h
@@ -3,42 +3,84 @@
 #include <stdbool.h>
 #include <pt.h>
 #include "sysex_handler.h"
+#include "midi_package.h"
+#include "midi_parser_channel_msg_callbacks.h"
 
-static struct pt task_midi_receiver__thread;
+
+static struct pt serial_midi_receiver__thread;
+static uint8_t serial_midi_receiver__running_status;
 
 void serial_midi_receiver__init() {
     Serial2.begin(31250);
-    PT_INIT(&task_midi_receiver__thread);
+    PT_INIT(&serial_midi_receiver__thread);
 }
 
 bool serial_midi_receiver__is_runnable() {
     return Serial2.available();
 }
 
-
+// no REALTIME support
 int serial_midi_receiver__run() {
-    struct pt *pt = &task_midi_receiver__thread;
+    struct pt *pt = &serial_midi_receiver__thread;
     PT_BEGIN(pt);
-    for (;;) {
-        uint8_t first = Serial2.read();
-        if (first != 0xF0) PT_RESTART(pt);
 
-        uint8_t b;
-        while (true) {
-            PT_YIELD(pt);
-            b = Serial2.read();
-
-            if (b >= 0x80) break;
-            sysex__data(b);
-        }
-
-        if (b == 0xF7) {
-            sysex__finish();
-        } else {
-            sysex__error();
-        }
-
-        PT_RESTART(pt);
+    int b = Serial2.read();
+    if (b < 0x80) {
+        PT_RESTART(pt);           // discard bytes 00..7F, because running status byte is not defined
     }
+
+    while (true) {
+        if (b < 0xF0) {
+            // channel messages payloads follow in groups of 1 or 2 bytes, depending on kind
+            serial_midi_receiver__running_status = b;
+
+            while (true) {
+                PT_YIELD(pt);
+                b = Serial2.read();
+                if (b >= 0x80) break;   // scrap current payload and re-parse as with this byte as beginning of the message
+
+                uint8_t p0 = b;
+                uint8_t p1 = 0;
+
+                // messages 0xCx and 0xDx have highest 3 bits = 110; they have 1-byte payloads..
+                if ((serial_midi_receiver__running_status & 0xE0U) != 0xC0U) {
+                    // for other messages, read extra byte
+                    PT_YIELD(pt);
+                    b = Serial2.read();
+                    if (b >= 0x80) break;   // scrap current payload and re-parse as with this byte as beginning of the message
+                    p1 = b;
+                }
+
+                midi_package_t midi_package;
+                midi_package.cin_cable = 0;
+                midi_package.evnt0 = serial_midi_receiver__running_status;
+                midi_package.evnt1 = p0;
+                midi_package.evnt2 = p1;
+                midi_channel_msg(midi_package);
+            }
+        } else {
+            // system common, system exclusive, or real-time messages
+
+            if (b == 0xF0) {    // sysex start
+                while (true) {
+                    PT_YIELD(pt);
+                    b = Serial2.read();
+                    if (b < 0x80) {
+                        sysex__data(b);
+                    } else {
+                        if (b == 0xF7) {
+                            sysex__finish();
+                        } else {
+                            sysex__error();
+                        }
+                        break;
+                    }
+                }
+            }
+
+            PT_RESTART(pt); // system common and system exclusive destroy running status
+        }
+    }
+
     PT_END(pt);
 }
