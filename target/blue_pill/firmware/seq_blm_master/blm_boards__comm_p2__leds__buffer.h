27d7d0e914ec8d877c4928f3da3384d51fefda74
semicontinuity <igor.a.karpov@gmail.com>
2020-03-07 11:47:48 +0300

    seq_blm_master: implement blm_boards__comm_p2__leds__buffer__blm_master__update methods (3)

--- target/blue_pill/firmware/seq_blm_master/blm_boards__comm_p2__leds__buffer.h
+++ target/blue_pill/firmware/seq_blm_master/blm_boards__comm_p2__leds__buffer.h
@@ -8,6 +8,9 @@
 #define __BLM_BOARDS__COMM_P2__LEDS__BUFFER
 
 #include <stdint.h>
+#include "debug_midi__arduino_serial.h"
+#include "debug_midi__sysex_parameters.h"
+
 
 uint8_t  blm_boards__comm_p2__leds__buffer__commands[16][16];     // every entry is a 1-byte command to be sent to a board
 uint16_t blm_boards__comm_p2__leds__buffer__commands__dirty[16];  // 1 bit means that corresponding command for the board is dirty
@@ -25,5 +28,24 @@ void blm_boards__comm_p2__leds__buffer__init() {
         blm_boards__comm_p2__leds__buffer__commands__dirty[i] = 0U;
     }
 }
+
+
+static void blm_boards__comm_p2__leds__buffer__blm_master__put_command(uint8_t matrix, uint8_t command_index, uint8_t command) {
+    debug_midi__sysex_p8(D_BOARD, matrix);
+    blm_boards__comm_p2__leds__buffer__commands[matrix][command_index] = command;
+    blm_boards__comm_p2__leds__buffer__commands__dirty[matrix] |= (1U << command_index);
+}
+
+
+void blm_boards__comm_p2__leds__buffer__blm_master__put_commands(
+    uint8_t base_matrix, uint8_t d, uint8_t dir, uint8_t coord, uint8_t pattern, uint8_t color_code)
+{
+    uint8_t pos = coord & 0x03U;
+    uint8_t command_base = (color_code << 7U) + (pos << 5U) + (dir << 4U);
+    uint8_t command_index = (pos << 2U) + (color_code << 1U) + (dir << 0U);
+    blm_boards__comm_p2__leds__buffer__blm_master__put_command(base_matrix + 0, command_index, command_base + (pattern & 0x0FU));
+    blm_boards__comm_p2__leds__buffer__blm_master__put_command(base_matrix + d, command_index, command_base + ((pattern >> 4U) & 0x0FU));
+}
+
 
 #endif
--- target/blue_pill/firmware/seq_blm_master/blm_boards__comm_p2__leds__buffer__blm_master_updates.h
+++ target/blue_pill/firmware/seq_blm_master/blm_boards__comm_p2__leds__buffer__blm_master_updates.h
@@ -27,12 +27,6 @@ void blm_boards__comm_p2__leds__buffer__blm_master__update_one(uint8_t row, uint
     debug_midi__sysex_p8(D_LED, led);
 }
 
-static void blm_boards__comm_p2__leds__buffer__blm_master__put_commands(uint8_t command_index, uint8_t matrix, uint8_t command) {
-    debug_midi__sysex_p8(D_BOARD, matrix);
-    blm_boards__comm_p2__leds__buffer__commands[matrix][command_index] = command;
-    blm_boards__comm_p2__leds__buffer__commands__dirty[matrix] |= (1U << command_index);
-}
-
 
 void blm_boards__comm_p2__leds__buffer__blm_master__update_row(uint8_t row, uint8_t is_second_half, uint8_t pattern, uint8_t color_code) {
     debug_midi__sysex_p0(D_UPDATE_ROW);
@@ -41,12 +35,7 @@ void blm_boards__comm_p2__leds__buffer__blm_master__update_row(uint8_t row, uint
 
     uint8_t matrix_y = row >> 2U;
     uint8_t base_matrix = (matrix_y << 2U) + (is_second_half ? 2 : 0);
-
-    uint8_t pos = row & 0x03U;
-    uint8_t command_base = (color_code << 7U) + (pos << 5U) + (0U << 4U);
-    uint8_t command_index = 1U << ((pos << 2U) + (color_code << 1U) + (0U << 0U));
-    blm_boards__comm_p2__leds__buffer__blm_master__put_commands(command_index, base_matrix + 0, command_base + (pattern & 0x0FU));
-    blm_boards__comm_p2__leds__buffer__blm_master__put_commands(command_index, base_matrix + 1, command_base + ((pattern >> 4U) & 0x0FU));
+    blm_boards__comm_p2__leds__buffer__blm_master__put_commands(base_matrix, 1, 0U, row, pattern, color_code);
 }
 
 void blm_boards__comm_p2__leds__buffer__blm_master__update_column(uint8_t column, uint8_t is_second_half, uint8_t pattern, uint8_t color_code) {
@@ -56,12 +45,7 @@ void blm_boards__comm_p2__leds__buffer__blm_master__update_column(uint8_t column
 
     uint8_t matrix_x = column >> 2U;
     uint8_t base_matrix = matrix_x + (is_second_half ? 8 : 0);
-
-    uint8_t pos = column & 0x03U;
-    uint8_t command_base = (color_code << 7U) + (pos << 5U) + (1U << 4U);
-    uint8_t command_index = 1U << ((pos << 2U) + (color_code << 1U) + (1U << 0U));
-    blm_boards__comm_p2__leds__buffer__blm_master__put_commands(command_index, base_matrix + 0, command_base + (pattern & 0x0FU));
-    blm_boards__comm_p2__leds__buffer__blm_master__put_commands(command_index, base_matrix + 4, command_base + ((pattern >> 4U) & 0x0FU));
+    blm_boards__comm_p2__leds__buffer__blm_master__put_commands(base_matrix, 4, 1U, column, pattern, color_code);
 }
 
 
