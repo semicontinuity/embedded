8d48d3be4cd21e443059cd48fff624c417b6f672
semicontinuity <igor.a.karpov@gmail.com>
2020-05-05 20:01:26 +0300

    console_i2c: support for start/stop/write byte/read byte commands

---
+++ target/examples/bsp/drivers/out/led2.h
@@ -0,0 +1,39 @@
+// =============================================================================
+// LED1 driver.
+// =============================================================================
+
+#ifndef __DRIVERS__OUT__LED2_H
+#define __DRIVERS__OUT__LED2_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "cpu/avr/gpio.h"
+
+
+/**
+ * Toggle LED2.
+ */
+inline void led2__toggle(void) {
+    TOGGLE(OUT__LED2);
+}
+
+
+/**
+ * Set LED2 value.
+ */
+inline void led2__set(const uint8_t value) {
+    if (value)
+        OUT_1(OUT__LED2);
+    else
+        OUT_0(OUT__LED2);
+}
+
+
+/**
+ * Initialize the LED2 line driver.
+ */
+inline void led2__init(void) {
+    USE_AS_OUTPUT(OUT__LED2);
+}
+
+#endif
