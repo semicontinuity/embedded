3fea28e0966077cda57355cdac34532a6bb81d81
semicontinuity <igor.a.karpov@gmail.com>
2020-07-04 23:54:00 +0300

    seq_blm_master: fixes to alive handler

--- target/blue_pill/firmware/seq_blm_master/blm_master__alive_handler.h
+++ target/blue_pill/firmware/seq_blm_master/blm_master__alive_handler.h
@@ -8,6 +8,7 @@
 
 
 bool blm_master__alive_handler__host_connected = false;
+uint32_t blm_master__alive_handler__last_rx_millis = 0;
 uint32_t blm_master__alive_handler__last_tx_millis = 0;
 
 
@@ -16,7 +17,7 @@ bool blm_master__alive_handler__is_host_connected() {
 }
 
 void blm_master__alive_handler__set_host_connected() {
-    blm_master__alive_handler__last_tx_millis = millis();
+    blm_master__alive_handler__last_rx_millis = millis();
     blm_master__alive_handler__host_connected = true;
     digitalWrite(PIN_LED_HOST_CONNECTED, PIN_LED_HOST_CONNECTED_ON);
 }
@@ -33,7 +34,11 @@ void blm_master__alive_handler__run() {
         blm_master__sysex_msg_sender__send_layout_info();
     }
     blm_master__alive_handler__last_tx_millis = millis();
-    blm_master__alive_handler__host_connected = false;
+    if (blm_master__alive_handler__host_connected) {
+        if (blm_master__alive_handler__last_tx_millis - blm_master__alive_handler__last_rx_millis > 5000) {
+            blm_master__alive_handler__host_connected = false;
+        }
+    }
     digitalWrite(PIN_LED_HOST_CONNECTED, PIN_LED_HOST_CONNECTED_OFF);
 }
 
