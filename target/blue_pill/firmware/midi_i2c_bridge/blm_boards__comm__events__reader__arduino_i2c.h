bf4a761d1b7f5425afa226c32b4d8679c772751d
semicontinuity <igor.a.karpov@gmail.com>
2020-07-05 12:23:29 +0300

    seq__blm_master: added testing firmware

---
+++ target/blue_pill/firmware/midi_i2c_bridge/blm_boards__comm__events__reader__arduino_i2c.h
@@ -0,0 +1,22 @@
+// Reads button and encoder events from BLM boards with I2C.
+// NB: Arduino I2C API is blocking, so communication will consume some time!
+// -----------------------------------------------------------------------------
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <stdint.h>
+
+
+static TwoWire *blm_boards__comm__events__arduino_i2c__wire;
+static uint8_t blm_boards__comm__events__arduino_i2c__base_address;
+
+
+void blm_boards__comm__events__arduino_i2c__init(TwoWire *wire, uint8_t base_address) {
+    blm_boards__comm__events__arduino_i2c__wire = wire;
+    blm_boards__comm__events__arduino_i2c__base_address = base_address;
+}
+
+uint8_t blm_boards__comm__events__arduino_i2c__read(uint8_t board) {
+    blm_boards__comm__events__arduino_i2c__wire->requestFrom(blm_boards__comm__events__arduino_i2c__base_address + board, 1);
+    return blm_boards__comm__events__arduino_i2c__wire->read();
+}
---
+++ target/blue_pill/firmware/midi_i2c_bridge/midi_sender.h
@@ -0,0 +1,38 @@
+#ifndef __MIDI_SENDER
+#define __MIDI_SENDER
+
+#include "midi_package.h"
+#include "midi_sender__arduino_usb_midi.h"
+
+
+// For serial:
+// - no running status optimisation
+// - only channel messages
+// - no locking: use from the loop thread only
+void midi_sender__send_package(midi_package_t p) {
+    midi_sender__arduino_usb_midi__send_package(p);
+}
+
+void midi_sender__program_change(uint8_t channel, uint8_t value) {
+    midi_package_t p = {.ALL = 0};
+    p.event = ProgramChange;
+    p.chn = channel;
+    p.value1 = value;
+    midi_sender__send_package(p);
+}
+
+void midi_sender__channel_pressure(uint8_t channel, uint8_t value) {
+    midi_package_t p = {.ALL = 0};
+    p.event = Aftertouch;
+    p.chn = channel;
+    p.value1 = value;
+    midi_sender__send_package(p);
+}
+
+
+void midi_sender__send_sysex_payload(uint8_t *payload, uint32 length) {
+    midi_sender__arduino_usb_midi__send_sysex_payload(payload, length);
+}
+
+
+#endif
