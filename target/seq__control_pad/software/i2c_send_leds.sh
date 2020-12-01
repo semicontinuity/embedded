c531ff808be133c19dcbe683b7169e55b2398bc0
semicontinuity <igor.a.karpov@gmail.com>
2020-12-01 11:19:13 +0300

    added debug info

--- target/seq__control_pad/software/i2c_send_leds.sh
+++ target/seq__control_pad/software/i2c_send_leds.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 BUS=$(i2cdetect -l | grep i2c-tiny-usb | cut -f1 | cut -c5-)
+echo "Detected i2c bus with i2c-tiny-usb: ${BUS}" >&2
 ADDRESS=$1
 sudo i2ctransfer -y ${BUS} w4@0x${ADDRESS} 0x${2} 0x${3} 0x${4} 0x${5}
