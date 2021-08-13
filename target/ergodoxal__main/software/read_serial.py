9123c684a1f48ceae189ce1e609bbcaebb341a53
semicontinuity <igor.a.karpov@gmail.com>
2021-08-13 19:41:47 +0300

    ergodoxal__main: added testbed connection description

---
+++ target/ergodoxal__main/software/read_serial.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import sys
+import serial
+import time
+import threading
+from threading import Thread
+
+if len(sys.argv) != 3:
+    print "Please supply COM port name and speed (/dev/ttyUSB0 38400)"
+    sys.exit()
+
+print sys.argv[1]
+port = serial.Serial()
+port.port = sys.argv[1]
+port.baudrate = int(sys.argv[2])
+port.timeout=1
+port.open()
+
+
+class PortReader(Thread):
+
+   def __init__ (self):
+      Thread.__init__(self)
+      self._stop = threading.Event()
+
+   def run(self):
+      while not self.stopped():
+        while port.inWaiting() > 0:
+          c = port.read(1)
+          print "%02X " % ord(c)
+
+   def stop(self):
+      self._stop.set()
+
+   def stopped(self):
+      return self._stop.isSet()
+
+
+try:
+    portReader = PortReader()
+    portReader.start()
+
+    sys.stdin.readline()
+    portReader.stop()
+    sys.exit()
+
+except (KeyboardInterrupt, SystemExit):
+    sys.exit()
+
