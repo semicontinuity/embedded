1f22592819a611d6ddf59d00fe76c1b58500be15
semicontinuity <igor.a.karpov@gmail.com>
2023-07-29 23:50:08 +0300

    test-10-contactor-controller (0)

---
+++ target/plc-4u-a/firmware/test-10-contactor-controller/valve_controller__1.h
@@ -0,0 +1,13 @@
+#ifndef VALVE_CONTROLER__H
+#define VALVE_CONTROLER__H
+
+
+bool valve_controller__1__is_runnable(void);
+void valve_controller__1__run(void);
+
+
+bool valve_controller__1__limit_switches_state_renderer__is_runnable(void);
+void valve_controller__1__limit_switches_state_renderer__run(void);
+
+
+#endif
