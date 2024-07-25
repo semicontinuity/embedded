bd079c99399086d45d9b1ee265b18c6fb5412d9d
semicontinuity <igor.a.karpov@gmail.com>
2024-07-25 14:20:13 +0300

    fx3u experiments

--- target/fx3u/test.py
+++ target/fx3u/test.py
@@ -28,16 +28,6 @@ import serial
 import time
 import threading
 from threading import Thread
-from crc16 import crc16
-
-
-#if len(sys.argv) != 3:
-#    print "Please supply COM port name and speed"
-#    sys.exit()
-
-
-#print sys.argv[1]
-#port = serial.Serial(sys.argv[1], int(sys.argv[2]))
 
 
 class PortReader(Thread):
@@ -60,14 +50,6 @@ class PortReader(Thread):
       return self._stop.isSet()
 
 
-
-#try:
-#    portReader = PortReader()
-#    portReader.start()
-#except (KeyboardInterrupt, SystemExit):
-#    sys.exit()
-
-
 def hex(n: int):
     return 0x30 + n if n < 10 else 0x41 + n - 10
 
@@ -81,21 +63,45 @@ def hi_nibble(n: int):
 
 
 class Fx3u:
-    def __init__(self, station_no: int = 1, plc_no: int = 0xFF, wait_time: int = 3):
+    def __init__(self, station_no: int = 1, plc_no: int = 0xFF, wait_time: int = 3, sum_check = False, cr_lf = False):
         self.station_no = station_no
         self.plc_no = plc_no
         self.wait_time = wait_time
+        self.sum_check = sum_check
+        self.cr_lf = cr_lf
 
-    def cmd_loopback_test(self, text: bytes):
-        data = bytearray(10 + len(text))
+    def _cmd_header(self, data, cmd_lo, cmd_hi):
         data[0] = 0x05
         data[1] = hex(hi_nibble(self.station_no))
         data[2] = hex(lo_nibble(self.station_no))
         data[3] = hex(hi_nibble(self.plc_no))
         data[4] = hex(lo_nibble(self.plc_no))
-        data[5] = ord('T')
-        data[6] = ord('T')
+        data[5] = ord(cmd_lo)
+        data[6] = ord(cmd_hi)
         data[7] = hex(self.wait_time)
+        return 8
+
+    def _cmd_footer(self, data, i):
+        if self.sum_check:
+            data[i] = 0x00
+            i += 1
+            data[i] = 0x00
+            i += 1
+
+        if self.cr_lf:
+            data[i] = 0x0D
+            i += 1
+            data[i] = 0x0A
+            i += 1
+
+    def _data_append(self, data, i, b):
+        data[i] = b
+        i += 1
+        return i
+
+    def cmd_loopback_test(self, text: bytes):
+        data = bytearray(10 + len(text) + (2 if self.sum_check else 0) + (2 if self.cr_lf else 0))
+        self._cmd_header(data, 'T', 'T')
         data[8] = hex(hi_nibble(len(text)))
         data[9] = hex(lo_nibble(len(text)))
 
@@ -104,17 +110,57 @@ class Fx3u:
             data[i] = b
             i += 1
 
+        self._cmd_footer(data, i)
+        return data
+
+    def cmd_batch_read_bits(self):
+        data = bytearray(8 + 6 + (2 if self.sum_check else 0) + (2 if self.cr_lf else 0))
+        i = self._cmd_header(data, 'B', 'R')
+        i = self._data_append(data, i, ord('Y'))
+        i = self._data_append(data, i, ord('0'))
+        i = self._data_append(data, i, ord('0'))
+        i = self._data_append(data, i, ord('0'))
+
+        i = self._data_append(data, i, ord('0'))
+        i = self._data_append(data, i, ord('4'))
+
+        self._cmd_footer(data, i)
         return data
 
 
-#port.write(data)
+port = serial.Serial(
+    "/dev/ttyUSB0",
+    baudrate=38400,
+    bytesize=7,
+    parity=serial.PARITY_EVEN
+)
 
 
-#sys.stdin.readline()
-#portReader.stop()
-#sys.exit()
+try:
+    portReader = PortReader(port)
+    portReader.start()
+except (KeyboardInterrupt, SystemExit):
+    sys.exit()
 
 
 fx3u = Fx3u()
-s = fx3u.cmd_loopback_test(b'ABCD')
+
+
+#s = fx3u.cmd_loopback_test(b'ABCD')
+s = fx3u.cmd_batch_read_bits()
 print(s)
+
+#port.write(b'\0x05\x0D\x0A')
+#time.sleep(0.1)
+
+port.write(s)
+
+time.sleep(1)
+
+port.write(s)
+
+
+sys.stdin.readline()
+portReader.stop()
+sys.exit()
+
