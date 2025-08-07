f6f7fcc4bbfa7de87c64221fb4f385f2b0ad0891
semicontinuity <igor.a.karpov@gmail.com>
2025-08-07 19:50:47 +0300

    fx3u += test2

---
+++ target/fx3u/test2.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+# Request:
+#  [ENQ]            1    05H
+
+import sys
+import threading
+from threading import Thread
+
+import serial
+
+
+class PortReader(Thread):
+
+   def __init__ (self, port):
+      Thread.__init__(self)
+      self._port = port
+      self._stop = threading.Event()
+
+   def run(self):
+      while not self.stopped():
+        while self._port.inWaiting() > 0:
+          c = self._port.read(1)
+          print("%02X " % ord(c))
+
+   def stop(self):
+      self._stop.set()
+
+   def stopped(self):
+      return self._stop.isSet()
+
+
+class Fx3u:
+    def __init__(self, station_no: int = 1, plc_no: int = 0xFF, wait_time: int = 3, sum_check = False, cr_lf = False):
+        self.station_no = station_no
+        self.wait_time = wait_time
+
+    def enq(self):
+        data = bytearray(1)
+        data[0] = 0x05
+        return data
+
+
+port = serial.Serial(
+    "/dev/ttyUSB0",
+    baudrate=38400,
+    bytesize=7,
+    parity=serial.PARITY_EVEN
+)
+
+
+try:
+    portReader = PortReader(port)
+    portReader.start()
+except (KeyboardInterrupt, SystemExit):
+    sys.exit()
+
+
+fx3u = Fx3u()
+
+
+s = fx3u.enq()
+print(s)
+
+port.write(s)
+
+
+sys.stdin.readline()
+portReader.stop()
+sys.exit()
+
